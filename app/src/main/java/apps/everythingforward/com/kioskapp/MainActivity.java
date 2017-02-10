8b5eee38a8d8837408171ba884c8bd71c41fe5ca
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-02-10 17:55:46 +0800

    Added UI elements

--- app/src/main/java/apps/everythingforward/com/kioskapp/MainActivity.java
+++ app/src/main/java/apps/everythingforward/com/kioskapp/MainActivity.java
@@ -1,13 +1,17 @@
 package apps.everythingforward.com.kioskapp;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.gc.materialdesign.views.Button;
@@ -18,19 +22,33 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
+import com.vstechlab.easyfonts.EasyFonts;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
+
+    //GoogleApiClient Necessary for using the Places API.
     private GoogleApiClient mGoogleApiClient;
     Button button;
     public int PLACE_PICKER_REQUEST = 1;
+    TextView welcomeTextView;
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //TODO: Implement the Places Search Option using the Places API.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        welcomeTextView = (TextView)findViewById(R.id.textView);
+        welcomeTextView.setTypeface(EasyFonts.robotoLightItalic(getApplicationContext()));
 
         mGoogleApiClient = new GoogleApiClient
                 .Builder(this)
@@ -55,6 +73,46 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 }
             }
         });
+
+
+        //All the code below is for the FAB. Methinks it's a good idea to replace the Buttons by an FAB. It looks Dope AF.
+
+        fab = (FloatingActionButton)findViewById(R.id.floatingActionButton);
+
+
+
+
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+// repeat many times:
+        ImageView itemIcon = new ImageView(this);
+        itemIcon.setImageDrawable(getResources().getDrawable(R.drawable.searchlogo));
+        SubActionButton button1 = itemBuilder.setContentView(itemIcon).build(); PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+                try {
+                    startActivityForResult(builder.build(MainActivity.this), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(button1)
+
+
+                .attachTo(fab)
+                .build();
+
 
     }
 
